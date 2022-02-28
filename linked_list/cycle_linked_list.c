da4c791064e34d09ee53cc8af323ad2e175b4d3c
wufeiwua <wufeiwua@126.com>
2022-02-28 22:43:27 +0800

    add: cycle doubly linked list

---
+++ linked_list/cycle_linked_list.c
@@ -0,0 +1,160 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "linked_list.h"
+typedef struct node
+{
+    Element element; // 当前元素
+    Position pre;    // 上一个节点
+    Position next;   // 下一个节点
+} Node;
+
+// 创建一个新表
+List MakeList()
+{
+    List L = (List)malloc(sizeof(Node));
+    L->pre = NULL;
+    L->next = NULL;
+    return L;
+}
+
+// 判断是否为空
+int IsEmpty(const List L)
+{
+    return L->next == NULL && L->pre == NULL;
+}
+
+// 判断是否为链表的最后一个元素
+// 循环链表，最后一个元素的 next 指向表头
+int IsLast(Position P, const List L)
+{
+    return P->next == L;
+}
+
+// 查找某个元素的位置
+Position Find(const Element E, const List L)
+{
+    Position P = L->next;
+    // 如果元素不相等继续向下找，如果相等了就返回
+    while (P != NULL && P->element != E)
+    {
+        P = P->next;
+        if (P = L)
+        {
+            // 找到表头还没有就退出
+            return NULL;
+        }
+    }
+    return P;
+}
+
+// 查找某个元素的上一个位置
+Position FindPrevious(Element E, List L)
+{
+    Position P = Find(E, L);
+    if (P == NULL)
+    {
+        return NULL;
+    }
+    return P->pre;
+}
+
+// 获取最后一个节点位置
+// 循环链表，表头的头指向最后一个元素
+Position FindLast(List L)
+{
+    Position P = L->pre;
+    if (P == NULL)
+    {
+        return L;
+    }
+    return P;
+}
+
+// 删除某个元素
+int Delete(Element E, List L)
+{
+    Position curNode = Find(E, L);
+    if (curNode == NULL)
+    {
+        // not in
+        return -1;
+    }
+    Position preNode = curNode->pre;
+    Position nextNode = curNode->next;
+    if (!IsLast(curNode, L))
+    {
+        preNode->next = curNode->next;
+        nextNode->pre = curNode->pre;
+    }
+    else
+    {
+        preNode->next == NULL;
+    }
+    free(curNode);
+    return 0;
+}
+
+// 插入一个元素到指定位置
+int InsertPositon(Element E, List L, Position P)
+{
+
+    if (P == NULL)
+    {
+        printf("cannot insert, position is not exist\n");
+        return -1;
+    }
+    Position newNode = (Position)malloc(sizeof(Node));
+    if (newNode == NULL)
+    {
+        // 没有足够的内存
+        return -1;
+    }
+    Position nextNode = P->next;
+    // first, nextNode is NULL, point self
+    if (nextNode == NULL)
+    {
+        nextNode = P;
+    }
+
+    // 插入中间位置
+    P->next = newNode;
+    newNode->pre = P;
+
+    newNode->element = E;
+    newNode->next = nextNode;
+
+    nextNode->pre = newNode;
+    return 0;
+}
+
+// 插入一个元素到链表尾端
+int Insert(Element E, List L)
+{
+    Position P = FindLast(L);
+    InsertPositon(E, L, P);
+    return 0;
+}
+
+Position Header(const List L);
+Position First(const List L);
+Position Advance(Position P);
+Element Retrieve(Position P);
+
+void PrintList(const List L, void (*pfunc)(Element E))
+{
+    printf("print list===\n");
+    Position cur = L->next;
+    while (cur != NULL && cur != L)
+    {
+        (*pfunc)(cur->element);
+        cur = cur->next;
+    }
+    printf("reverse===\n");
+    cur = L->pre;
+    while (cur != NULL && cur != L)
+    {
+        (*pfunc)(cur->element);
+        cur = cur->pre;
+    }
+}
